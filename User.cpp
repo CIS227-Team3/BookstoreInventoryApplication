ca23294ea9cbed9afc77ca4511775d1d46bcfa5a
Connor Schaffer - GRCC <123031274+cschafferGRCC@users.noreply.github.com>
2023-01-30 18:28:24 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -7,33 +7,40 @@
 
 #include "User.h"
 
+//Constructor
 User::User() {
 	username = "none";
 	password = "none";
 
 }
 
+//Makes a User
 User::User(string username, string password) {
 	this->username = username;
 	this->password = password;
 }
 
+//Sets Username
 void User::setUsername(string username) {
 	this->username = username;
 }
 
+//Gets Username
 string User::getUsername() {
 	return username;
 }
 
+//Sets Password
 void User::setPassword(string password) {
 	this->password = password;
 }
 
+//Gets Password
 string User::getPassword() {
 	return password;
 }
 
+//Updates Password
 void User::updatePassword(string username, string password) {
 
 }
