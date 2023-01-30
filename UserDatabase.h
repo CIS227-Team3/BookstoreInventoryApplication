28cedb9d7e58dc14505d3dca0f0262efa9d7c745
AliciaRogers <122655372+AliciaRogers@users.noreply.github.com>
2023-01-29 21:20:07 -0500

    Update UserDatabase.h

--- UserDatabase.h
+++ UserDatabase.h
@@ -9,10 +9,15 @@
 class UserDatabase {
 public:
     UserDatabase();
+    void addUser();
+    void updateUser();
 
 private:
     vector<User> Users;
     void addInitialUsers();
+    bool searchUser(string username); // searches for user based on username
+    bool validUser(string username, string password); // validates that user is in database and password matches
+    void updateUserDatabase();
 };
 
 
