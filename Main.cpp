10b9343a9e84c12225a1604e4fade8ea3672ebfa
Connor Schaffer - GRCC <123031274+cschafferGRCC@users.noreply.github.com>
2023-02-14 16:50:30 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -31,69 +31,88 @@ int main() {
     BookstoreInventory inventory;
     UserDatabase users;
 
-    short unsigned int menuOption;
+    short unsigned int menuOption1;
+    short unsigned int menuOption2;
     string title;
 
-    cout << "---------------------------" << endl;
-    cout << "Thank you for using BOOKIN!" << endl;
-    cout << "---------------------------" << endl;
-    cout << endl;
-    cout << "Please type in your username" << endl;
-    cin >> username;
+    menuOption1 = 1; // starts the menu at 1 to allow user to enter a title to search
+    while (menuOption1 != 0 ) {
+        switch (menuOption1) { 
+            case 1:
+                cout << "---------------------------" << endl;
+                cout << "Thank you for using BOOKIN!" << endl;
+                cout << "---------------------------" << endl;
+                cout << endl;
 
-    cout << "Please type in your password" << endl;
-    cin >> password;
+                cout << "Press 0 to exit application." << endl;
+                cout << "Press 1 restart the program." << endl;
+                cout << "Press 2 to login." << endl;
+            case 2:
+                cout << "Please type in your username:" << endl;
+                cin >> username;
+                cout << "Please type in your password:" << endl;
+                cin >> password;
 
-    // removes newline character from buffer
-    cin.clear();
-    cin.ignore(1000, '\n');
+                // removes newline character from buffer
+                cin.clear();
+                cin.ignore(1000, '\n');
 
-    if (users.loginUser(username, password)) {
-        menuOption = 1; // starts the menu at 1 to allow user to enter a title to search
-        while (menuOption != 0) {
-            switch (menuOption) {
-                case 1:
-                    cout << "Press 0 to exit application." << endl;
-                    cout << "Press 1 to bring up this menu again." << endl;
-                    cout << "Press 2 to search for a book." << endl;
-                    cout << "Press 3 to add a book." << endl;
-                    cout << "Press 4 to add a description/plot and genre." << endl;
-                case 2:
-                    cout << "Please enter a title to search: " << endl;
-                    getline(cin, title);
-                    inventory.searchForBook(title);
-                    break;
-                case 3:
-                    cout << "Please add the following information. " << endl;
-                    cout << "ISBN, Book-Title, Book-Author, Year-Of-Publication, Publisher: " << endl;
-                    cout << "Add ISBN: " << endl;
-                    //cin ISBN
-                    cout << "Add Book-Title: " << endl;
-                    //cin Book-Title
-                    cout << "Add Book-Author: " << endl;
-                    //cin Book-Author
-                    cout << "Add Year-Of-Publication: " << endl;
-                    //cin Year-Of-Publication
-                    cout << "Add Publisher: " << endl;
-                    //cin Publisher
-                    //add book to list
-                    break;
-                case 4:
-                    cout << "Please enter ISBN number: " << endl;
-                    //cin ISBN number
-                    cout << "Please enter description/plot: " << endl;
-                    //cin ISBN number
-                    cout << "Please enter genre: " << endl;
-                    //cin ISBN number
-                    //add it to a csv
-                    break;
-                default:
-                    cout << "Menu option not recognized." << endl;
-                    break;
+                 if (users.loginUser(username, password)) {
+                    menuOption2 = 1; // starts the menu at 1 to allow user to enter a title to search
+                    while (menuOption2 != 0) {
+                    switch (menuOption2) {
+                        case 1:
+                            cout << "Press 0 to exit application." << endl;
+                            cout << "Press 1 to bring up this menu again." << endl;
+                            cout << "Press 2 to search for a book." << endl;
+                            cout << "Press 3 to add a book." << endl;
+                            cout << "Press 4 to add a description/plot and genre." << endl;
+                        case 2:
+                            cout << "Please enter a title to search: " << endl;
+                            getline(cin, title);
+                            inventory.searchForBook(title);
+                            break;
+                        case 3:
+                            cout << "Please add this information." << endl;
+                            cout << "ISBN, Book-Title, Book-Author, Year-Of-Publication, Publisher: " << endl;
+                            cout << "Add ISBN: " << endl;
+                            //cin ISBN
+                            cout << "Add Book-Title: " << endl;
+                            //cin Book-Title
+                            cout << "Add Book-Author: " << endl;
+                            //cin Book-Author
+                            cout << "Add Year-Of-Publication: " << endl;
+                            //cin Year-Of-Publication
+                            cout << "Add Publisher: " << endl;
+                            //cin Publisher
+                            //add book to list
+                            break;
+                        case 4:
+                            cout << "Please enter ISBN number: " << endl;
+                            //cin ISBN number
+                            cout << "Please enter description/plot: " << endl;
+                            //cin ISBN number
+                            cout << "Please enter genre: " << endl;
+                            //cin ISBN number
+                            //add it to a csv
+                            break;
+                        default:
+                            cout << "Menu option not recognized." << endl;
+                            break;
+                        }
+                        menuOption2 = promptUserInput();
+                    }
+                } else {
+                    cout << "User is not recognized!" << endl;
+                    menuOption1 = promptUserInput();
+                }
+            default:
+                cout << "Menu option not recognized." << endl;
+                break;
             }
-            menuOption = promptUserInput();
+            menuOption1 = promptUserInput();
         }
-    }
+
 
     cout << "Exiting BOOKIN. Goodbye!" << endl;
 
