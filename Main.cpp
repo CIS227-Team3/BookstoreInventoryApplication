37615f19ae9c926e9d6c8a3328c0e63cf2b66b9c
AliciaRogers <122655372+AliciaRogers@users.noreply.github.com>
2023-02-15 07:31:18 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,149 @@
 /*
- * readFile.h
+ * Main.cpp
  *
  *  Created on: Jan 25, 2023
  *      Author: Group 3
  */
 
-#ifndef READFILE_H_
-#define READFILE_H_
-
-#include "includes/rapidcsv.h"
-#include "includes/json.hpp"
-#include "includes/sqlite3.h"
-
 #include <iostream>
-#include <fstream>
-#include <list>
-#include "Book.h"
-#include "User.h"
+#include <iomanip>
+#include <string>
+
+#include "UserDatabase.h"
+//#include "UserDatabase.cpp"
 #include "BookstoreInventory.h"
+//#includes "BookstoreInventory.cpp"
+#include "Book.h"
+// #include "Book.cpp"
+#include "fileOperations.h"
+// #include "readFile.cpp"
+#include "User.h"
+// #include "User.cpp"
+#include "menuOptions.h"
+// #include "menuOptions.cpp"
 
 using namespace std;
 
-void readBooksFile(string filePath, deque<Book> &Inventory);
+int main() {
+    string password;
+    string username;
 
-void writeBooksFile(deque<Book> &UserList);
+    BookstoreInventory inventory;
+    UserDatabase users;
+    User currentUser;
+    Book foundBook;
 
-void readUsersFile(list<User> &Users);
+    short unsigned int menuOption;
+    string title;
 
+    string ISBN;
+	string author;
+	int year;
+	string publisher;
+	string description;
+	string genre;
 
-#endif /* READFILE_H_ */
+    cout << "---------------------------" << endl;
+    cout << "Thank you for using BOOKIN!" << endl;
+    cout << "---------------------------" << endl;
+    cout << endl;
+    cout << "Please type in your username" << endl;
+    cin >> username;
+
+    cout << "Please type in your password" << endl;
+    cin >> password;
+
+    // removes newline character from buffer
+    cin.clear();
+    cin.ignore(1000, '\n');
+
+    if (users.loginUser(username, password)) {
+        currentUser = users.getCurrentUser();
+        menuOption = 1; // starts the menu at 1 for menu
+        while (menuOption != 0) {
+            switch (menuOption) {
+                case 1:
+                    // do nothing so the menu will prompt on loop
+                    break;
+                case 2:
+                    cout << "Please enter a title to search: " << endl;
+                    getline(cin, title);
+                    inventory.searchForBook(title);
+                    break;
+                case 3:
+                {
+                    cout << "Please add the following information. " << endl;
+                    cout << "ISBN, Book-Title, Book-Author, Year-Of-Publication, Publisher: " << endl;
+                    cout << "Add ISBN: " << endl;
+                    getline(cin, ISBN);
+                    cout << "Add Book-Title: " << endl;
+                    getline(cin, title);
+                    cout << "Add Book-Author: " << endl;
+                    getline(cin, author);
+                    cout << "Add Year-Of-Publication: " << endl;
+                    year = validateYear(); // validates that the year entered is an integer
+                    cout << "Add Publisher: " << endl;
+                    getline(cin, publisher);
+
+                    // By default, user does not have to enter a description or a genre at the time it is added to the database
+                    Book book(ISBN, title, author, year, publisher, "", "");
+
+                    // adds book to inventory
+                    inventory.addBook(book);
+
+                    break;
+                }
+                case 4:
+                {
+					cout << "Please enter title of book to delete:" << endl;
+					getline(cin, title);
+					inventory.deleteBook(title);
+					break;
+				}
+                case 5:
+                {
+                    cout << "Please enter title: " << endl;
+                    getline(cin, title);
+                    cout << "Please enter description/plot: " << endl;
+                    getline(cin, description);
+                    cout << "Please enter genre: " << endl;
+                    getline(cin, genre);
+                    
+                    // updates the description and genre of the specified book.
+                    inventory.updateDescription(title, description);
+                    inventory.updateGenre(title, genre);
+                    break;
+                }
+                case 6:
+                {
+                    cout << "Please enter title of book to add:" << endl;
+                    getline(cin, title);
+                    foundBook = inventory.searchForBook(title);
+                    currentUser.addToUserList(foundBook);
+                    break;
+                }
+
+                case 7:
+                {
+                    currentUser.listUserList();
+                    break;
+                }
+                case 8:
+                {
+                    inventory.exportInventoryToCsv();
+                    break;
+                }
+                default:
+                {
+                    cout << "Menu option not recognized." << endl;
+                    break;
+                }
+            }
+            menuOption = menuOptions();
+        }
+    }
+
+    cout << "Exiting BOOKIN. Goodbye!" << endl;
+
+    return EXIT_SUCCESS;
+}
