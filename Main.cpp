65ad38471652d58058449aad6084f12e6abbec87
AliciaRogers <122655372+AliciaRogers@users.noreply.github.com>
2023-02-15 01:29:03 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,114 +1,31 @@
 /*
- * Main.cpp
+ * readFile.h
  *
  *  Created on: Jan 25, 2023
  *      Author: Group 3
  */
 
-#include <iostream>
-#include <iomanip>
-#include <string>
+#ifndef READFILE_H_
+#define READFILE_H_
 
-#include "UserDatabase.h"
-//#include "UserDatabase.cpp"
-#include "BookstoreInventory.h"
-//#includes "BookstoreInventory.cpp"
+#include "includes/rapidcsv.h"
+#include "includes/json.hpp"
+#include "includes/sqlite3.h"
+
+#include <iostream>
+#include <fstream>
+#include <list>
 #include "Book.h"
-// #include "Book.cpp"
-#include "fileOperations.h"
-// #include "readFile.cpp"
 #include "User.h"
-// #include "User.cpp"
-#include "menuOptions.h"
-// #include "menuOptions.cpp"
+#include "BookstoreInventory.h"
 
 using namespace std;
 
-int main() {
-    string password;
-    string username;
+void readBooksFile(string filePath, deque<Book> &Inventory);
 
-    BookstoreInventory inventory;
-    UserDatabase users;
-    User currentUser;
-    Book foundBook;
+void writeBooksFile(deque<Book> &UserList);
 
-    short unsigned int menuOption;
-    string title;
+void readUsersFile(list<User> &Users);
 
-    cout << "---------------------------" << endl;
-    cout << "Thank you for using BOOKIN!" << endl;
-    cout << "---------------------------" << endl;
-    cout << endl;
-    cout << "Please type in your username" << endl;
-    cin >> username;
 
-    cout << "Please type in your password" << endl;
-    cin >> password;
-
-    // removes newline character from buffer
-    cin.clear();
-    cin.ignore(1000, '\n');
-
-    if (users.loginUser(username, password)) {
-        currentUser = users.getCurrentUser();
-        menuOption = 1; // starts the menu at 1 for menu
-        while (menuOption != 0) {
-            switch (menuOption) {
-                case 1:
-                    // do nothing so the menu will prompt on loop
-                    break;
-                case 2:
-                    cout << "Please enter a title to search: " << endl;
-                    getline(cin, title);
-                    inventory.searchForBook(title);
-                    break;
-                case 3:
-                    cout << "Please add the following information. " << endl;
-                    cout << "ISBN, Book-Title, Book-Author, Year-Of-Publication, Publisher: " << endl;
-                    cout << "Add ISBN: " << endl;
-                    //cin ISBN
-                    cout << "Add Book-Title: " << endl;
-                    //cin Book-Title
-                    cout << "Add Book-Author: " << endl;
-                    //cin Book-Author
-                    cout << "Add Year-Of-Publication: " << endl;
-                    //cin Year-Of-Publication
-                    cout << "Add Publisher: " << endl;
-                    //cin Publisher
-                    //add book to list
-                    break;
-                case 4:
-                    cout << "Please enter ISBN number: " << endl;
-                    //cin ISBN number
-                    cout << "Please enter description/plot: " << endl;
-                    //cin ISBN number
-                    cout << "Please enter genre: " << endl;
-                    //cin ISBN number
-                    //add it to a csv
-                    break;
-                case 5:
-                    cout << "Please enter title of book to add:" << endl;
-                    getline(cin, title);
-                    foundBook = inventory.searchForBook(title);
-                    currentUser.addToUserList(foundBook);
-                    break;
-                case 6:
-                    currentUser.listUserList();
-                    break;
-                case 7:
-                    inventory.exportInventoryToCsv();
-                    break;
-                default:
-                    cout << "Menu option not recognized." << endl;
-                    break;
-            }
-            menuOption = menuOptions();
-        }
-    }
-
-    cout << "Exiting BOOKIN. Goodbye!" << endl;
-
-    return EXIT_SUCCESS;
-}
-
+#endif /* READFILE_H_ */
